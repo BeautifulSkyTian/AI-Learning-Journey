4f3b294bd8c5b11cc016b556e96cec3a5cd03f00
Tianqi Pan <pantianqi666@gmail.com>
2026-05-30 20:16:25 -0400

    not too much done for Day 4

---
+++ Day4.py
@@ -0,0 +1,15 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model = "gemini-3.5-flash", 
+    contents = "Hi there! What is your name?"
+)
+
+print(response.text)
